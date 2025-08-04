b34593de9d0eed1cf27f20e7ffd1b1c17913ecad
chetan28-demo <chetanmallah85@gmail.com>
2025-08-04 15:23:08 +0530

    Updated CircleCardStyles.js

--- src/Components/CircleCard/CircleCardStyles.js
+++ src/Components/CircleCard/CircleCardStyles.js
@@ -1,38 +1,3 @@
-// // circleCardStyles.js
-// import { StyleSheet } from 'react-native';
-
-// export const styles = StyleSheet.create({
-//   cardContainer: {
-//     paddingVertical: 10,
-//   },
-//   circleCard: {
-//     width: 100,
-//     height: 130,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     borderRadius: 20, // Rounded corners for the whole card
-//     borderWidth: 2,
-//     borderColor: '#ddd',
-//     backgroundColor: '#fff',
-//     marginHorizontal: 10, // Space between cards
-//     elevation: 5, // Shadow for Android
-//     shadowColor: '#000', // iOS Shadow
-//     shadowOffset: { width: 0, height: 4 },
-//     shadowOpacity: 0.2,
-//     shadowRadius: 6,
-//   },
-//   circleImage: {
-//     width: 70,
-//     height: 70,
-//     borderRadius: 35, // Makes the image circular
-//   },
-//   circleText: {
-//     fontSize: 14,
-//     color: '#333',
-//     marginTop: 8,
-//     textAlign: 'center',
-//   },
-// });
 
 
 import { StyleSheet } from 'react-native';
